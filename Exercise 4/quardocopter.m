9147a22fc940b7bf74e30fef6c422ad937eaac5a
tanya525625 <tanya_postcrossing@mail.ru>
2019-05-04 19:51:08 +0300

    The final version of the quardocopter

--- Exercise 4/quardocopter.m
+++ Exercise 4/quardocopter.m
@@ -3,12 +3,12 @@ weight = 3;
 timespan = [0 50];
 z_initial = 0;
 
-coeffs = fminsearch(@(coeffs) J(z_initial, height, coeffs, timespan, mass), [0 0]);
+coeffs = fminsearch(@(coeffs) J(z_initial, height, coeffs, timespan,weight), [0 0]);
 
 kp = coeffs(1);
 kd = coeffs(2);
 
-[t,z] = ode45(@(t,z) movement_equation(kp, kd, z, height, mass), timespan, [z_initial; 0]);
+[t,z] = ode45(@(t,z) movement_equation(kp, kd, z, height, weight), timespan, [z_initial; 0]);
 hold on;
 plot(t,z(:,1));
 
@@ -16,15 +16,15 @@ function res = smoothness(z, u, z_change)
     res = (z(:,1)-z_change).^2 + z(:,2).^2 + u.^2;
 end
 
-function dydt = movement_equation(kp, kd, z, z_change, mass)
-    dydt = [z(2);(-kp.*(z(1)-z_change)-kd.*z(2))./mass];
+function dydt = movement_equation(kp, kd, z, z_change, weight)
+    dydt = [z(2);(-kp.*(z(1)-z_change)-kd.*z(2))./weight];
 end
 
-function res = J(z_initial, z_change, coeffs, time_span, mass)
+function res = J(z_initial, z_change, coeffs, time_span, weight)
     kp = coeffs(1);
     kd = coeffs(2);
-    [t,z] = ode45(@(t,z) movement_equation(kp, kd, z, z_change, mass), time_span, [z_initial; 0]);
-    u = T(kp, kd, z, z_change, mass);
+    [t,z] = ode45(@(t,z) movement_equation(kp, kd, z, z_change, weight), time_span, [z_initial; 0]);
+    u = T(kp, kd, z, z_change, weight);
     values = smoothness(z, u, z_change);
     res = 0;
     for i = 2 : length(t)
@@ -32,6 +32,6 @@ function res = J(z_initial, z_change, coeffs, time_span, mass)
     end
 end
 
-function res = T(kp, kd, z, z_change, mass)
-    res = kp.*(z(:,1)-z_change) + kd.*z(:,2) + mass.*9.8;
+function res = T(kp, kd, z, z_change, weight)
+    res = kp.*(z(:,1)-z_change) + kd.*z(:,2) + weight.*9.8;
 end
